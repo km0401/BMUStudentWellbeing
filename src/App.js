b1b4bcaf23976b81781537879b6f5415ae644808
km0401 <87297544+km0401@users.noreply.github.com>
2023-03-27 22:43:46 +0530

    Blogs carousel added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import HeroBanner from './HeroBanner';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Box } from '@mui/material';
+import BlogCarousel from './BlogCarousel';
+// import BlogCard from './BlogCard';
 
 class App extends Component{
     render(){
@@ -14,6 +16,7 @@ class App extends Component{
     }}>
 <Header/>
 <HeroBanner/>
+<BlogCarousel/>
 </Auth0Provider>
       )
     }
