b9bdc9554580146da935dec573cebc8231d6d983
km0401 <87297544+km0401@users.noreply.github.com>
2023-03-29 13:02:28 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React,{Component} from 'react';
 import Header from "./Header";
 import HeroBanner from './HeroBanner';
 import { Auth0Provider } from "@auth0/auth0-react";
-<<<<<<< HEAD
 import BlogCarousel from './BlogCarousel';
+import AboutUs from "./About Us";
 import MeetOurDoctors from './MeetOurDoctors';
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography:{
+      fontFamily:'Poppins',
+    button: {
+      textTransform: 'none'
+    }
+  }
+})
+
 class App extends Component{
     render(){
       return (
+        <ThemeProvider theme={theme}>
         <Auth0Provider
           domain='dev-404dtec71j6qzjx6.us.auth0.com'
           clientId='gBPCm4tysf1C0kUl2qUT4O2wj4S1LoV3'
@@ -15,31 +28,13 @@ class App extends Component{
             redirect_uri: window.location.origin,
           }}
         >
-          <Header />
-          <HeroBanner />
+          <Header/>
+          <HeroBanner/>
+          <AboutUs/>
           <MeetOurDoctors/>
           <BlogCarousel />
         </Auth0Provider>
-=======
-import { Box } from '@mui/material';
-import BlogCarousel from './BlogCarousel';
-import AboutUs from './About Us';
-// import BlogCard from './BlogCard';
-
-class App extends Component{
-    render(){
-      return(
-        <Auth0Provider domain="dev-404dtec71j6qzjx6.us.auth0.com"
-    clientId="gBPCm4tysf1C0kUl2qUT4O2wj4S1LoV3"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}>
-<Header/>
-<HeroBanner/>
-<AboutUs/>
-<BlogCarousel/>
-</Auth0Provider>
->>>>>>> 586527e7ea732dfe9678ea75b54dae6c40ef17d9
+        </ThemeProvider>
       )
     }
   }
